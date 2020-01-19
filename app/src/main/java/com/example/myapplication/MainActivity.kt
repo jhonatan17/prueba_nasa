981e86248b7b5ab923755a3fcbf3f9066eb9c3ac
Jhonatan <jhonatangolden5@gmail.com>
2020-01-19 18:12:47 -0500

    sd

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.Toast
 import com.android.volley.*
-import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,64 +13,101 @@ import org.json.JSONException
 import org.json.JSONObject
 import rx.Observable
 import rx.Subscriber
-import java.lang.reflect.Array
 import java.util.*
+import android.content.SharedPreferences
+import android.widget.ArrayAdapter
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var PRIVATE_MODE = 0
+    private val PREF_NAME = "mindorks-welcome"
 
+    lateinit var  sharedPref: SharedPreferences ;
+    lateinit var  editor:SharedPreferences.Editor
+    val historial :ArrayList<String> = ArrayList()
     val imageId :ArrayList<String> = ArrayList()
-    val title:ArrayList<String> = ArrayList()
-    val n:ArrayList<String> = ArrayList()
+    val data:ArrayList<JSONObject> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         sharedPref = getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        editor = sharedPref.edit()
       mi()
-
-
+        Toast.makeText(this,sharedPref.getString("historial", ""),Toast.LENGTH_LONG).show()
+       if(sharedPref.getString("historial", null)!=null){
+        var h= JSONArray(sharedPref.getString("historial", ""))
+        for(i in 0 until h.length()){
+            historial.add(h[i].toString())
+       }
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, historial)
+        input.setAdapter(adapter)}
+        close.setOnClickListener(View.OnClickListener {
+            contentDes.visibility=View.GONE
+        })
     }
 
-    fun mi(){
-      button.setOnClickListener(View.OnClickListener {
-        //  Toast.makeText(this, ""+getUsers(),Toast.LENGTH_SHORT).show();
+    fun mi() {
+        button.setOnClickListener(View.OnClickListener {
+            if(!input.text.toString().equals("")){
+            if (!historial.contains(input.text.toString())) {
+                historial.add(input.text.toString())
+                editor.putString("historial", historial.toString())
+                editor.apply()
+                val adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, historial)
+                input.setAdapter(adapter)
+            }
+            //  Toast.makeText(this, ""+getUsers(),Toast.LENGTH_SHORT).show();
             //val intent=Intent(this,MapsActivity::class.java)
-getUsers().subscribe({
+            getUsers().subscribe({
 
 
+                input.setText("")
+                var array = JSONArray(it.toString())
+                imageId.clear();
+                data.clear()
+                for (i in 0 until array.length()) {
+                    JSONObject(array[i].toString()).get("data");
 
-var array= JSONArray(it.toString())
-imageId.clear();
-    title.clear();
-    n.clear();
-    for (i in 0 until array.length()) {
-        JSONObject(array[i].toString()).get("data");
+                    try {
+                        var img =
+                            JSONObject(JSONArray(JSONObject(array[i].toString()).get("links").toString())[0].toString()).get(
+                                "href"
+                            ).toString()
+                        var datap =
+                            JSONObject(JSONArray(JSONObject(array[i].toString()).get("data").toString())[0].toString());
 
-        try {
-            var img=JSONObject(JSONArray(JSONObject(array[i].toString()).get("links").toString())[0].toString()).get("href").toString()
-           img = img.replace( oldValue = " ",
-                        newValue = "%20")
-            imageId.add(img )
-            title.add("sd");
-            n.add("s");
-        } catch (e: JSONException) {
+                        img = img.replace(
+                            oldValue = " ",
+                            newValue = "%20"
+                        )
+                        imageId.add(img)
+                        data.add(datap)
+                    } catch (e: JSONException) {
 
-        }
+                    }
 
-    }
-    val myListAdapter = MyListAdapter(this,n,title,imageId)
-    listView.adapter = myListAdapter
-    listView.setOnItemClickListener(){adapterView, view, position, id ->
-        val itemAtPos = adapterView.getItemAtPosition(position)
-        val itemIdAtPos = adapterView.getItemIdAtPosition(position)
-        Toast.makeText(this, "Click on item at $itemAtPos its item id $itemIdAtPos", Toast.LENGTH_LONG).show()
-    }
+                }
+                val myListAdapter = MyListAdapter(this, imageId)
+                grid.adapter = myListAdapter
+                grid.setOnItemClickListener() { adapterView, view, position, id ->
+                    web.loadUrl(imageId[position])
+                    val itemAtPos = data[position]
+                    contentDes.visibility = View.VISIBLE
+                    titleimg.setText(itemAtPos.get("title").toString())
+                    descripcion.setText(itemAtPos.get("description").toString())
+                    fecha.setText(itemAtPos.get("date_created").toString())
+                    Toast.makeText(this, "ds", Toast.LENGTH_LONG).show()
+
+                }
 
 
+            })}else
+                Toast.makeText(this,"El campo de busqueda no puede estar vacio",Toast.LENGTH_SHORT).show()
+            // startActivity(intent)
+        })
 
-})
-       // startActivity(intent)
-            })
     }
 
     fun  getUsers():Observable<Any> {
@@ -82,7 +116,7 @@ imageId.clear();
 
     return Observable.create(object : Observable.OnSubscribe<Any> {
             override fun call(subscriber: Subscriber<in Any>) {
-                val my_url = "https://images-api.nasa.gov/search?q="+editText4.text
+                val my_url = "https://images-api.nasa.gov/search?q="+input.text
                 var respuesta="";
 
                 val requ = object : JsonObjectRequest(Method.GET, my_url, null,
--- app/src/main/java/com/example/myapplication/list.kt
+++ app/src/main/java/com/example/myapplication/list.kt
@@ -5,21 +5,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.squareup.picasso.Picasso
+import org.json.JSONObject
 
-class MyListAdapter(private val context: Activity, private val title: ArrayList<String>, private val description: ArrayList<String>, private val imgid: ArrayList<String>)
-    : ArrayAdapter<String>(context, R.layout.list_view,imgid) {
+class MyListAdapter(private val context: Activity,private val imgid: ArrayList<String>)
+    : ArrayAdapter<String>(context, R.layout.list_view, imgid)  {
 
     override fun getView(position: Int, view: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.list_view, null, true)
 
-        val titleText = rowView.findViewById(R.id.title) as TextView
         val imageView = rowView.findViewById(R.id.icon) as ImageView
-        val subtitleText = rowView.findViewById(R.id.description) as TextView
-        Picasso.with(context).load(imgid[position]).into(imageView)
-        titleText.text = title[position]
-        subtitleText.text = description[position]
+
+        Picasso.with(context).load(imgid[position]).fit().into(imageView)
 
         return rowView
     }
+
 }
